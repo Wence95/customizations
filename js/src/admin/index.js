79cbb428f604908bd7a430381ef5249e048b2294
Pablo <pab.bp95@gmail.com>
2024-12-19 04:21:46 -0300

    new customizations. don't use this.

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,5 +1,7 @@
 import { extend } from 'flarum/common/extend';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
+import AdminNav from 'flarum/admin/components/AdminNav';
+
 
 app.initializers.add('hospital-clinico-puq-customizations', () => {
   console.log("EXTENSION NAMEasd is working!");
@@ -19,5 +21,44 @@ app.initializers.add('hospital-clinico-puq-customizations', () => {
     } catch (e) {
       console.error('Failed to remove ExtensionInfo:', e);
     }
+  });
+  extend(AdminNav.prototype, 'extensionItems', function (extensionItems) {
+    // Example: Remove the "Settings" menu item
+    console.log(extensionItems);
+    let extensionsList = ['extension-flarum-approval',
+      'extension-flarum-bbcode',
+      'extension-flarum-emoji',
+      'extension-flarum-flags',
+      'extension-fof-pages',
+      'extension-afrux-forum-widgets-core',
+      'extension-flarum-likes',
+      'extension-flarum-lock',
+      'extension-flarum-markdown',
+      'extension-flarum-mentions',
+      'extension-flarum-nicknames',
+      'extension-flarum-pusher',
+      'extension-flamarkt-signup-page',
+      'extension-flarum-sticky',
+      'extension-flarum-subscription',
+      'extension-flarum-suspend',
+      'extension-hospital-clinico-puq-login-logs',
+      'extension-hospital-clinico-puq-user-views',
+      'extension-clarkwinkelmann-who-read',
+      'category-theme',
+      'extension-blomstra-welcome-login',
+      'category-language',
+      'extension-flarum-lang-english',
+      'extension-flarum-lang-spanish'];
+    for (let extensionsListKey in extensionsList) {
+      console.log(extensionsList[extensionsListKey]);
+      extensionItems.remove(extensionsList[extensionsListKey]);
+    }
+
+    // You can similarly remove other items by their key names
+    // For instance, to remove the "Extensions" item:
+    // items.remove('extensions');
+
+    // If you want to remove custom items added by other extensions:
+    // items.remove('custom-item-key');
   });
 });
